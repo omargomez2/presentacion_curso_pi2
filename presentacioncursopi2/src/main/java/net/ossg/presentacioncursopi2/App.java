3e2c87bc7c5f31f625856df9bd0a3e9d0f8f8cd4
SYSTEMarket <SYSTEMarket@LAPTOP-0GOQ4EG5>
2020-07-08 16:22:56 -0500

    Modificacion 1

--- presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
+++ presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello Course!" );
         System.out.println( "This is Omar and likes to listen EDM");
+        System.out.println( "This is Aldo and likes to listen Alternative Music");
     }
 }
