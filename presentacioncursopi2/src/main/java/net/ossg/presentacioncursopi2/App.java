735cd039de00d0baddd4ac3d00253ec2219015ab
franj <franj@LAPTOP-DIRSTBTR>
2020-07-08 16:26:22 -0500

    Hola :v

--- presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
+++ presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello Course!" );
         System.out.println( "This is Omar and likes to listen EDM");
         System.out.println( "This is Aldo and likes to listen Alternative Music");
+        System.out.println( "This is Francisco and likes to listen Electronic Music");
     }
 }
