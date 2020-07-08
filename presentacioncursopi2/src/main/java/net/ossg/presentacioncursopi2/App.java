91291a9d1849cb9fbf722583cd1a14af0ce2c03d
alvar <alvar@LAPTOP-DBQ1O88B>
2020-07-08 16:41:59 -0500

    ya valió

--- presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
+++ presentacioncursopi2/src/main/java/net/ossg/presentacioncursopi2/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println( "This is Francisco and likes to listen Electronic Music");
         System.out.println ("This is Erik and likes to isten WWW");
         System.out.println( "This is Samuel and she doesn't love you");
+        System.out.println( "This is Alvaro and likes to play video games");
     }
 }
